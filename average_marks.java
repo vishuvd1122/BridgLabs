e1a3636ef9a6043bb5ad2d605c2a8ffe76017f9c
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-06-11 11:42:07 +0530

    second question

---
+++ average_marks.java
@@ -0,0 +1,11 @@
+public class average_marks {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry= 100;
+
+        //Finding the average
+        int average = (maths+physics+chemistry)/3;
+        System.out.println(average);
+    }
+}
